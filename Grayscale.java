a0444145c72dca6581d5322af7cb9a427cd59863
joebuscus <158345506+joebuscus01@users.noreply.github.com>
2025-03-22 18:20:49 -0400

    Add files via upload

--- Grayscale.java
+++ Grayscale.java
@@ -1,10 +1,41 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import javax.swing.*;
 
 
 public class Grayscale extends Converter {
 
-    public void convert(String inputFileName, String outputFileName) throws IOException {
+    public void convert(String inputFileName, String outputFileName) throws IOException { //throws exception if issue with input/output
+    	File inputFile = new File("toronto.png");
+    	System.out.println("inputFile" + inputFile);
+    	BufferedImage originalImage = ImageIO.read(inputFile); //reads input
+    	
+    	if (originalImage != null) {
+    		display(originalImage);
+    	}
+    	
+    	
+    	
+    	File outputFile = new File(outputFileName);
+    	// ImageIO.write(processedImage, "PNG", outputFile); //output	
 
+    }
+    
+    //displays img using Swing 
+    //DELETE WITH IMPORT IN FINAL COPY
+    public static void display(BufferedImage img) {
+    	System.out.println("Displayed Image: ");
+    	//NOT MY CODE
+    	JFrame frame = new JFrame();
+	    JLabel label = new JLabel();
+		frame.setSize(img.getWidth(), img.getHeight());
+		label.setIcon(new ImageIcon(img));
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		// NOT MY CODE
     }
 
 }

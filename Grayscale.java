883dafebf30fd6f5457d3215fd07ff6577848234
joebuscus <158345506+joebuscus01@users.noreply.github.com>
2025-03-25 21:33:35 -0400

    Add files via upload

--- Grayscale.java
+++ Grayscale.java
@@ -8,11 +8,39 @@ import javax.swing.*;
 public class Grayscale extends Converter {
 
     public void convert(String inputFileName, String outputFileName) throws IOException { //throws exception if issue with input/output
+    	
+    	System.out.println("Converting to Grayscale. . .");
     	File inputFile = new File("toronto.png");
     	System.out.println("inputFile" + inputFile);
     	BufferedImage originalImage = ImageIO.read(inputFile); //reads input
+    	BufferedImage grayimage = new BufferedImage(originalImage.getWidth(),originalImage.getHeight(), BufferedImage.TYPE_BYTE_GRAY); //blank gray image with original height & image
+    	int rgb = 0, r = 0, g = 0, b = 0;
     	
-    	if (originalImage != null) {
+    	//Think of image as pixels in rows & cols
+    	for (int y = 0; y< originalImage.getHeight(); y++) { //each row
+    		
+    		for (int x = 0; x < originalImage.getWidth(); x++) { //width
+    			// grab rgb
+    			// seperate r, g, b
+    			// add r, g, b & divide by 3
+    			
+    			rgb = originalImage.getRGB(x, y);
+    			r = ( (rgb >> 16 ) & 0xFF);
+    			g = ( (rgb >> 8 ) & 0xFF);
+    			b = ( rgb & 0xFF);
+    			rgb = ( (int) r + g + b)/3;
+    			rgb = 225 << 24 | rgb << 16 | rgb << 8 | rgb; 
+    			grayimage.setRGB(x, y, rgb);
+    			
+    		
+    			
+    		}
+    		
+    	}
+    	
+    	
+    	
+    	if (originalImage != null) { //DELTE AT END
     		display(originalImage);
     	}
     	
